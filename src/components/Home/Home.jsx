0691a1fdfd031b68e5e8a8afd80455c894cc8e66
altomizawa <alyssontomizawa@hotmail.com>
2024-03-16 11:06:22 -0300

    clean on Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,9 @@
-import { useContext } from 'react'
-
 import Header from '../Header/Header'
 import Hero from '../Hero/Hero'
 import AboutUs from '../AboutUs/AboutUs'
 import OurTradition from '../OurTradition/OurTradition'
 
-import { AuthContext } from '../Context/AuthContext'
 export default function Home() {
-    // const {isLoggedIn} = props;
-
-    // // const {userContextData, isLoggedIn} = useContext(UserContext)
-    // const {isLoggedIn} = useContext(AuthContext);
-
     return (
         <> 
             <Header />
