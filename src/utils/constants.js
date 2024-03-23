5c1717165515da18f51813789d90c3548390c629
altomizawa <alyssontomizawa@hotmail.com>
2024-03-23 11:39:19 -0300

    change baseurl

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-// const BASE_URL = 'http://localhost:3000';
-const BASE_URL = 'https://food-ordering-backend-7jj7.onrender.com';
+const BASE_URL = 'http://localhost:3000';
+// const BASE_URL = 'https://food-ordering-backend-7jj7.onrender.com';
+// const BASE_URL = 'https://italiarestaurant.mooo.com';
 
 export { BASE_URL };
