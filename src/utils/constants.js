36130af41a88aecb45649b21ebfb7bd3488231b1
Al Tomizawa <alyssontomizawa@hotmail.com>
2024-03-24 12:13:05 -0300

    change api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:3000';
+// const BASE_URL = 'http://localhost:3000';
 // const BASE_URL = 'https://food-ordering-backend-7jj7.onrender.com';
-// const BASE_URL = 'https://italiarestaurant.mooo.com';
+const BASE_URL = 'http://api.italiarestaurant.mooo.com';
 
 export { BASE_URL };
