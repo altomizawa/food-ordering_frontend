07264b02206f1c21bde122c9a165718829573132
Al Tomizawa <alyssontomizawa@hotmail.com>
2024-03-25 15:53:32 -0300

    Changed BASE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-// const BASE_URL = 'http://localhost:3000';
-// const BASE_URL = 'https://food-ordering-backend-7jj7.onrender.com';
-const BASE_URL = 'https://italianrestaurant.fairuse.org';
+// const BASE_URL = 'https://food-ordering-backend-7jj7.onrender.com'; // this server takes 50s to start up for the first time when idle for too long
+const BASE_URL = 'https://italianrestaurant.fairuse.org'; // Had a problem with nginx + certbot so https://api.italianrestaurant.fairuse.org is not secure
 
 export { BASE_URL };
