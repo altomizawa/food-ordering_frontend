25b02a463612ed8cf360b81d6abf0b43a7af5b2d
Al Tomizawa <alyssontomizawa@hotmail.com>
2024-03-26 20:51:47 -0300

    delete old api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-// const BASE_URL = 'https://food-ordering-backend-7jj7.onrender.com'; // this server takes 50s to start up for the first time when idle for too long
 const BASE_URL = 'https://api.italianrestaurant.fairuse.org';
 
 export { BASE_URL };
