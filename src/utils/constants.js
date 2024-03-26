cb2223aca1ec91f1718a27f50ea96fa3bf9e6ff1
Al Tomizawa <alyssontomizawa@hotmail.com>
2024-03-26 20:40:15 -0300

    changed api to final api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 // const BASE_URL = 'https://food-ordering-backend-7jj7.onrender.com'; // this server takes 50s to start up for the first time when idle for too long
-const BASE_URL = 'https://italianrestaurant.fairuse.org'; // Had a problem with nginx + certbot so https://api.italianrestaurant.fairuse.org is not secure
+const BASE_URL = 'https://api.italianrestaurant.fairuse.org';
 
 export { BASE_URL };
