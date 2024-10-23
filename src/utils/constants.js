308382046aab197b81fd3bb54ef44f506e162ea0
altomizawa <alyssontomizawa@hotmail.com>
2024-10-23 18:20:07 -0300

    refactor: update BASE_URL in constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-// const BASE_URL = 'https://api.italianrestaurant.fairuse.org';
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://api.italianrestaurant.fairuse.org';
+// const BASE_URL = 'http://localhost:3000';
 export { BASE_URL };
